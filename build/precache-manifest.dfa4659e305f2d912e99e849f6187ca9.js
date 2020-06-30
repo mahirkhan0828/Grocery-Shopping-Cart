38d782a29ca4e5ae83b9140da32fee76d151cb0f
Mahir Khan <mahirkhan0828@Mahirs-MacBook-Air.local>
2020-06-30 10:18:07 -0500

    Added build

---
+++ build/precache-manifest.dfa4659e305f2d912e99e849f6187ca9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b64ba4d12680b67f3a77f5e489eafe5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48db8474c2bc920f0a23",
+    "url": "/static/css/main.359aa141.chunk.css"
+  },
+  {
+    "revision": "5b2541d92188393fa01b",
+    "url": "/static/js/2.72490d8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.72490d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48db8474c2bc920f0a23",
+    "url": "/static/js/main.22ce195b.chunk.js"
+  },
+  {
+    "revision": "a898010a964e1853f20f",
+    "url": "/static/js/runtime-main.93e8652b.js"
+  },
+  {
+    "revision": "b6b30ef23267fa91f85888da6480a768",
+    "url": "/static/media/NutritionBackgroundImage.b6b30ef2.jpg"
+  }
+]);
